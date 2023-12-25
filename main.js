aba21faea606e0e33163ca951971b012c8cf52df
thiraphat.ch <thiraphat.ch@rmuti.ac.th>
2023-12-25 18:23:38 +0700

    Added a sound effect to the counter app when the '+' or '-' buttons are clicked. (generated by blackbox.ai )

--- main.js
+++ main.js
@@ -1,46 +1,50 @@
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
+const audio = new Audio();
+audio.src = "./pop.mp3";
 function MyApp() {
-  const [sum, setSum] = React.useState(0);
+    const [sum, setSum] = React.useState(0);
 
-  const updateSum = (n) => {
-    setSum(() => sum + n);
-  };
+    const updateSum = (n) => {
+        setSum(() => sum + n);
+    };
 
-  return (
-    <div>
-      <h1>Hello World!</h1>
-      <p>Sum = {sum}</p>
-      <button onClick={()=> alert('Coming soon...')}>ADD Counter</button>
-      <div>
-        <Counter updateSum={updateSum} />
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <p>Sum = {sum}</p>
+            <button onClick={() => alert('Coming soon...')}>ADD Counter</button>
+            <div>
+                <Counter updateSum={updateSum} />
+            </div>
+        </div>
+    );
 }
 
 function Counter({ updateSum }) {
-  const [count, setCount] = React.useState(0);
+    const [count, setCount] = React.useState(0);
 
-  const updateCounter = (n) => {
-    if (count + n < 0) {
-      return;
-    }
-    setCount(() => count + n);
-    updateSum(n); // Update the sum when the counter changes
-  };
+    const updateCounter = (n) => {
+        if (count + n < 0) {
+            return;
+        }
+        setCount(() => count + n);
+        updateSum(n);
+    };
 
-  return (
-    <div className="counter">
-      <div className="controller">
-        <button onClick={() => updateCounter(1)}>+</button>
-        <button>{count}</button>
-        <button onClick={() => updateCounter(-1)}>-</button>
-        <button onClick={() => updateCounter(-count)}>C</button>
-        <button>X</button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="counter">
+            <div className="controller">
+                <button onClick={() => {
+                    updateCounter(1);
+                    audio.play();
+                }}>+</button>
+                <button>{count}</button>
+                <button onClick={() => {updateCounter(-1); audio.play();}}>-</button>
+                <button onClick={() => updateCounter(-count)}>C</button>
+                <button>X</button>
+            </div>
+        </div>
+    );
 }
 
 root.render(<MyApp />);
